eb3976b94916c94e9f1ad4c0a88a7279492989e7
Vaughn Walker <vaughn.walker@integral.io>
2019-04-10 22:10:36 -0400

    Verify spare counts bonus roll.

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,26 @@
 class Game {
 
-    private int score = 0;
+    private int[] scorePerRoll = new int[21];
+    private int rollIndex = 0;
+    private int[] frame = new int[10];
 
     int score() {
+        int score = 0;
+
+        for (int frameIndex = 0, roll = 0; frameIndex < frame.length; frameIndex++) {
+            if (scorePerRoll[roll] + scorePerRoll[roll + 1] == 10) {
+                score += 10 + scorePerRoll[roll + 2];
+                roll += 2;
+            } else {
+                score += scorePerRoll[roll] + scorePerRoll[roll + 1];
+                roll += 2;
+            }
+        }
         return score;
     }
 
     void roll(int downedPins) {
-        score += downedPins;
+        scorePerRoll[rollIndex] = downedPins;
+        rollIndex++;
     }
 }
--- test/GameTest.java
+++ test/GameTest.java
@@ -26,9 +26,21 @@ public class GameTest {
     }
 
     @Test
-    public void rollAllOnes_scoreReturnsTwenty() {
+    public void rollAllOnes_scoreReturns20() {
         for (int i = 0; i < 20; i++) {
             game.roll(1);
+        }
+        assertEquals(20, game.score());
+    }
+
+    @Test
+    public void rollSpareFollowedByFive_scoreReturns20() {
+        game.roll(1);
+        game.roll(9);
+        game.roll(5);
+
+        for (int i = 0; i < 17; i++) {
+            game.roll(0);
         }
         assertEquals(20, game.score());
     }
