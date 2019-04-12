c7cb04ecdf4ac767ca8fd90368ca6840d65d3047
Vaughn Walker <vaughn.walker@integral.io>
2019-04-12 14:33:43 -0400

    Reset game and verify that pins knocked down add to score.

--- src/Game.java
+++ src/Game.java
@@ -1,37 +1,11 @@
 class Game {
-
-    private int[] scorePerRoll = new int[21];
-    private int rollIndex = 0;
-    private int[] frame = new int[10];
+    private int score = 0;
 
     int score() {
-        int score = 0;
-
-        for (int frameIndex = 0, roll = 0; frameIndex < frame.length; frameIndex++) {
-            if (isSpare(roll)) {
-                score += 10 + scorePerRoll[roll + 2];
-                roll += 2;
-            } else if (isStrike(roll)) {
-                score += 10 + scorePerRoll[roll + 1] + scorePerRoll[roll + 2];
-                roll++;
-            } else {
-                score += scorePerRoll[roll] + scorePerRoll[roll + 1];
-                roll += 2;
-            }
-        }
         return score;
     }
 
-    void roll(int downedPins) {
-        scorePerRoll[rollIndex] = downedPins;
-        rollIndex++;
-    }
-
-    private boolean isSpare(int roll) {
-        return scorePerRoll[roll] + scorePerRoll[roll + 1] == 10;
-    }
-
-    private boolean isStrike(int roll) {
-        return scorePerRoll[roll] == 10;
+    void roll(int pinsDowned) {
+        score += pinsDowned;
     }
 }
--- test/GameTest.java
+++ test/GameTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class GameTest {
+
     private Game game;
 
     @Before
@@ -12,83 +13,17 @@ public class GameTest {
     }
 
     @Test
-    public void initializeGameWithScoreOfZero() {
+    public void gameInitializes_scoreReturns0() {
         assertEquals(0, game.score());
     }
 
     @Test
-    public void rollZeroGame() {
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
-
-        assertEquals(0, game.score());
-    }
-
-    @Test
-    public void rollAllOnes_scoreReturns20() {
-        for (int i = 0; i < 20; i++) {
-            game.roll(1);
-        }
-        assertEquals(20, game.score());
-    }
-
-    @Test
-    public void rollSpareFollowedByFive_scoreReturns20() {
-        game.roll(1);
-        game.roll(9);
-        game.roll(5);
-
-        for (int i = 0; i < 17; i++) {
-            game.roll(0);
-        }
-        assertEquals(20, game.score());
-    }
-
-    @Test
-    public void roll10SparesAnd1pin_scoreReturns110() {
-        for (int i = 0; i < 10; i++) {
-            game.roll(1);
-            game.roll(9);
-        }
-        game.roll(1);
-
-        assertEquals(110, game.score());
-    }
-
-    @Test
-    public void rollStrikeFollowedByOneAndOne_scoreReturns14() {
-        game.roll(10);
-        game.roll(1);
-        game.roll(1);
-
-        for (int i = 0; i < 17; i++) {
-            game.roll(0);
-        }
-        assertEquals(14, game.score());
-    }
-
-    @Test
-    public void perfectGame() {
+    public void rollCompleteGameOf1PinRolls_scoreReturns20() {
         int i = 0;
-        while (i < 12) {
-            game.roll(10);
+        while (i < 20) {
+            game.roll(1);
             i++;
         }
-
-        assertEquals(300, game.score());
-    }
-
-    @Test
-    public void rollAll1PinLastFrame3Strikes_scoreReturns39() {
-        for (int i = 0; i < 9; i++) {
-            game.roll(1);
-            game.roll(0);
-        }
-        game.roll(10);
-        game.roll(10);
-        game.roll(10);
-
-        assertEquals(39, game.score());
+        assertEquals(20, game.score());
     }
 }
